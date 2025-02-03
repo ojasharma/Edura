23b6fb86ee468cddb0ae326cfb1fe7fa70282d14
Savy <savy.novachez011@proton.me>
2025-02-04 00:18:13 +0530

    backend: add cors middleware

--- backend-ts/src/index.ts
+++ backend-ts/src/index.ts
@@ -1,7 +1,11 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
+import { cors } from 'hono/cors'
+
 const app = new Hono()
+
+app.use("/api/*", cors())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
