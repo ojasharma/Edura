32810f6f5693b530eed030098c36e42136cfc186
ojassharma <ojasvisharmacs@gmail.com>
2025-04-16 12:29:19 +0530

    rewrite

--- TODO.md
+++
@@ -1,8 +0,0 @@
-### Front End
-- [ ] Add Frontend tasks here...
-
-### Back End
-- [ ] [Hono.js](https://hono.dev/) Rewrite
-
-### Misc.
-- [ ] Add Misc. tasks here...
---
+++ z_frontend_oldjs/src/App.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./components/ThemeContext";
+import Layout from "./components/Layout";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import ClassDetails from "./pages/ClassDetails";
+import VideoDetails from "./pages/VideoDetails";
+import ProfilePage from "./pages/ProfilePage"; // Import the new ProfilePage component
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Layout>
+              <Signup />
+            </Layout>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/class/:id"
+          element={
+            <Layout>
+              <ClassDetails />
+            </Layout>
+          }
+        />
+        <Route
+          path="/video/:videoId"
+          element={
+            <Layout>
+              <VideoDetails />
+            </Layout>
+          }
+        />
+        {/* Add the new Profile route */}
+        <Route
+          path="/profile"
+          element={
+            <Layout>
+              <ProfilePage />
+            </Layout>
+          }
+        />
+      </Routes>
+    </ThemeProvider>
+  );
+}
+
+export default App;
